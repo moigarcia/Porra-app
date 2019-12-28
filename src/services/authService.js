bbebf635ef908da404704e8b2bd7b11947e67151
Moises Garcia <moisesgarcia83@gmail.com>
2019-12-28 10:40:24 +0100

    changes

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,39 +3,43 @@ import { constants } from "../utils/constants";
 
 let userLocal = localStorage.getItem(constants.CURRENT_USER_KEY);
 
-const authenticate = () => {
-  fetch("https://porra-api.herokuapp.com/auth/login/success", {
-    method: "GET",
-    credentials: "include",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Credentials": true
-    }
-  })
-    .then(response => {
-      if (response.status === 200) return response.json();
-      throw new Error("failed to authenticate user");
-    })
-    .then(responseJson => {
+const authenticate = async () => {
+  try {
+    const response = await fetch(
+      "https://porra-api.herokuapp.com/auth/login/success",
+      {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true
+        }
+      }
+    );
+
+    if (response.status === 200) {
+      const responseJson = response.json();
       localStorage.setItem(
         constants.CURRENT_USER_KEY,
         JSON.stringify(responseJson)
       );
       return JSON.stringify(responseJson);
-    })
-    .catch(error => {
-      throw error;
-    });
-};
-const getUserById = async (id) => { 
-  try {
-    const response = await http.get(`/users/${id}`)
-    return response.data
-  } catch(error){
-    throw error
+    } else {
+      throw new Error("failed to authenticate user");
+    }
+  } catch (error) {
+    throw error;
   }
-}
+};
+const getUserById = async id => {
+  try {
+    const response = await http.get(`/users/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 const logOut = () => {
   fetch("https://porra-api.herokuapp.com/auth/logout", {
@@ -48,14 +52,14 @@ const logOut = () => {
     }
   })
     .then(response => {
-      console.log("entra")
+      console.log("entra");
       userLocal = {};
       localStorage.removeItem(constants.CURRENT_USER_KEY);
       return userLocal;
     })
     .catch(error => {
       throw error;
-    });  
+    });
 };
 
 export default {
